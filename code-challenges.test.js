c37a8517a057e101e4a5b9281d403f16aad41fd3
Charlie19255 <charlie19255@gmail.com>
2022-11-23 20:55:59 -0800

    finished

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -13,40 +13,176 @@
 // --------------------1) Create a function that takes in a string and returns a string with a coded message. The coded message converts "a" to 4, "e" to 3, "i" to 1, and "o" to 0.
 
 // a) Create a test with expect statements using the variables provided.
+describe("changer", () => {
+    it("takes in a string and returns a string with a coded message", () => {
+        const secretCodeWord1 = "Lackadaisical"
+        const output1 = "L4ck4d41s1c4l"
+        const secretCodeWord2 = "Gobbledygook"
+        const output2 = "G0bbl3dyg00k"
+        const secretCodeWord3 = "Eccentric"
+        const output3 = "3cc3ntr1c"
+      expect(changer(secretCodeWord1)).toEqual(output1)
+      expect(changer(secretCodeWord2)).toEqual(output2)
+      expect(changer(secretCodeWord3)).toEqual(output3)
+    })
+  })
 
-const secretCodeWord1 = "Lackadaisical"
-// Expected output: "L4ck4d41s1c4l"
-const secretCodeWord2 = "Gobbledygook"
-// Expected output: "G0bbl3dyg00k"
-const secretCodeWord3 = "Eccentric"
-// Expected output: "3cc3ntr1c"
+// ReferenceError: changer is not defined
 
 // b) Create the function that makes the test pass.
+
+//create a function called changer
+//will take the parameter of string
+  //function will go through each letter of the string
+  //the function will change each vowel to a specific number
+  //will return the new string
+//test function
+
+const changer = (string) => {
+    const newString = []
+    for(i=0; i<string.length; i++){
+        if(string[i]==='a') {newString.push(string[i].replace("a",4))
+        }
+        else if(string[i]==='A') {newString.push(string[i].replace("A",4))
+        }
+        else if(string[i]==='e') {newString.push(string[i].replace("e",3))
+        }
+        else if(string[i]==='E') {newString.push(string[i].replace("E",3))
+        }
+        else if(string[i]==='i') {newString.push(string[i].replace("i",1))
+        }
+        else if(string[i]==='I') {newString.push(string[i].replace("I",1))
+        }
+        else if(string[i]==='o') {newString.push(string[i].replace("o",0))
+        }
+        else if(string[i]==='O') {newString.push(string[i].replace("O",0))
+        }
+        else {newString.push(string[i])}
+    }
+    return newString.join("")
+}
+
+
+// Test Suites: 1 passed, 1 total
+// Tests:       1 passed, 1 total
 
 // --------------------2) Create a function that takes in an array of words and a single letter and returns an array of all the words containing that particular letter.
 
 // a) Create a test with expects statement using the variable provided.
 
-const fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"]
+describe("finder", () => {
+    it("takes in an array of words and a single letter and returns an array of all the words containing that particular letter", () => {
+        const fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"]
 
-const letterA = "a"
-// Expected output: ["Mango", "Apricot", "Peach"]
-const letterE = "e"
-// Expected output: ["Cherry", "Blueberry", "Peach"]
+        const letterA = "a"
+        output4 = ["Mango", "Apricot", "Peach"]
+        const letterE = "e"
+        output5 = ["Cherry", "Blueberry", "Peach"]
+      expect(finder(fruitArray, letterA)).toEqual(output4)
+      expect(finder(fruitArray, letterE)).toEqual(output5)
+    })
+  })
+
+
+//   ReferenceError: finder is not defined
 
 // b) Create the function that makes the test pass.
+
+//create a function called finder
+//it will take the parameter of array and letter
+    //create a new variable called newArray. From the result of using map to lowercase all strings
+    // using the filter method we can find which strings in the array has the letter
+    //use map to uppercase the strings again
+    //return the array with strings that only has the letter
+//test function
+
+// const finder = (array, letter) => {
+    
+//    return array.filter(value=> value.includes(letter))
+// }
+
+// - Expected  - 1
+// + Received  + 0
+
+const finder = (array, letter) => {
+    let newArray = array.map(value=>value.toLowerCase())
+    return newArray.filter(value=> value.includes(letter)).map(value=>value.charAt(0).toUpperCase() + value.slice(1))
+}
+
+// Test Suites: 1 passed, 1 total
+// Tests:       2 passed, 2 total
 
 // --------------------3) Create a function that takes in an array of 5 numbers and determines whether or not the array is a "full house". A full house is exactly one pair and one three of a kind.
 
 // a) Create a test with expect statements using the variable provided.
 
-const hand1 = [5, 5, 5, 3, 3]
-// Expected output: true
-const hand2 = [5, 5, 3, 3, 4]
-// Expected output: false
-const hand3 = [5, 5, 5, 5, 4]
-// Expected output: false
-const hand4 = [7, 2, 7, 2, 7]
-// Expected output: true
+
+describe("fullHouse", () => {
+    it("takes in an array of 5 numbers and determines whether or not the array is a full house", () => {
+        const hand1 = [5, 5, 5, 3, 3]
+        let output6 = true
+        const hand2 = [5, 5, 3, 3, 4]
+        let output7 = false
+        const hand3 = [5, 5, 5, 5, 4]
+        let output8 = false
+        const hand4 = [7, 2, 7, 2, 7]
+        let output9 = true
+      expect(fullHouse(hand1)).toEqual(output6)
+      expect(fullHouse(hand2)).toEqual(output7)
+      expect(fullHouse(hand3)).toEqual(output8)
+      expect(fullHouse(hand4)).toEqual(output9)
+    })
+  })
+
+//   ReferenceError: fullHouse is not defined
 
 // b) Create the function that makes the test pass.
+
+//create a function called fullHouse
+//it will take in the parameter of an array
+//create an object to keep count
+  //create a for loop where it looks at the value at each index of the array
+  //the object will keep count for each value found in the for loop starting at 0 and adding 1 for each one found
+  //create a variable to check the object count of each value
+  // an if statement will check if the values of the object will pass as a full house by checking whether each one is equal to a count of 3 and 2.
+  //if it does then it returns true
+  //if not then it returns false
+//test the function
+
+// const fullHouse = (array) => {
+    
+//     for(i=0;i<array.length;i++){
+//         for(j=0;j<array.length;j++)
+//         if(array.indexOf(array[i], 3) && array.indexOf(array[j],2)){
+//             return true
+//         }
+//         else {return false}
+//     }
+// }
+// Expected: false
+// Received: true
+// const fullHouse = (array) => {
+//   let check = array.slice(0,1)
+//   for(i=0;i<array.length;i++)
+//     if(array[i]===check)
+//         {return true}
+//     else {return false}
+// }
+
+// Expected: true
+// Received: false
+
+const fullHouse = (array) => {
+    let countObj = {}
+    for(let i of array){
+      countObj[i] = (countObj[i]|| 0 )+1;
+    }
+    let count = Object.values(countObj)
+    if((count[0] === 2 && count[1] === 3) || (count[1] === 2 && count[0] === 3)){
+      return true;
+    }
+    return false;
+  }
+
+//   Test Suites: 1 passed, 1 total
+// Tests:       3 passed, 3 total
--- interview-questions.md
+++ interview-questions.md
@@ -8,44 +8,44 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What does calling super do in a class?
 
-Your answer:
+Your answer: Calling super in a class means that the class is a child of another class. The child class is able to inherit methods and characteristics of the parent class. The super allows for creating an object in the child class and passing arguments to the parent class to attain the methods and characteristics as described before. This allows for more universal ability of the parent class allowing for more child classes to be more distinct while reusing past code.
 
-Researched answer:
+Researched answer: It is used to pass arguments implicitly to the parent class. It is used to access the method of the parent class while maintaining the individuality of a child class. Utilizing just the super key word without any paretheses will result in an argument error. If no arguments are required for the parent class, then the super keyword must be called with an empty parentheses to pass properly.
 
 2. What is a gem?
 
-Your answer:
+Your answer: A gem is a package used to install Rspec. This is necessary since Rspec is used to test Ruby code. Without the gem installer, Ruby code would not be able to be trial run without going through actual coding. This is a result of test driven development which is efficient in making code that corresponds with what the developer wants to achieve.
 
-Researched answer:
+Researched answer: A gem is a file containing a lib directory, test directory, rakefile, bin, documentation, and a gemspec. It has a version, and tests that are utilized for the framework the developer uses. The gem is used to allow the developer to use the code within without directly inserting the code. This is because gems can be installed off the internet allowing developers to use as neccessary to test different frameworks.
 
 3. What is a relational database? Are there other kinds of databases?
 
-Your answer:
+Your answer: A relational database is a bank of information that is able to correlate inputs and retrieve the appropriate information. There are many different kinds of databases that correspond with different API's which have many uses to their information. For an example, NASA has an open source API for the public to use which has data contained about the stars and planets other than our own.
 
-Researched answer:
+Researched answer: A relational database is a set of tables containing columns and rows. The tables are used to house the data that is matched up with pre-defined relationships. Some other types of databases are centralized, cloud, commercial, personal, open-source, end-user, graph, NoSQL, object-oriented, operational, and distributed. Each has their own purpose, such as centralized is in one location usually for universities or companies located in a central computer containing the database.
 
 4. What are primary keys? Why are they important?
 
-Your answer:
+Your answer: Primary keys are a notation device. It is used to indicate that it belongs to one string of information. This is so that strings of information that have the same name do not correspond with each other mixing up the data and making it hard to read. Primary keys allow us to differentiate between data even if the information is exactly the same.
 
-Researched answer:
+Researched answer: They are unique identifiers for each table record. They can be used to quickly look through a table for a piece of data. Only one primary key is assigned to one table. They are important to relational databases as a table may contain many records in which duplicates occur. It is imperative that the primary key is utilized to differentiate the two duplicates from each other.
 
 5. What are the HTTP verbs? What is each verb's corresponding CRUD action?
 
-Your answer:
+Your answer: The HTTP verbs are put, post, delete, patch, and pull. Put and patch correspond with the update action allowing to change data as required. The create is associated with the post action, allowing users to make a new data appear. The pull is the read action pair meaning that users can ascertain information to take in. The delete action and the delete are the same to get rid of unneccessary information.
 
-Researched answer:
+Researched answer: The HTTP verbs are get, head, post, put, delete, connect, options, trace, and patch. They are request methods used to define what actions are to be performed. They each correspond to one of the four CRUD actions. Create is related to post, connect, and trace. Read is related to get, head, and options. Update is related to put and patch. Delete is related to delete.
 
 ## Looking Ahead: Terms for Next Week
 
 Research and define the following terms to the best of your ability.
 
-1. RESTful routes:
+1. RESTful routes: standards used in different languages for time-efficient and reusable pathways
 
-2. Ruby on Rails:
+2. Ruby on Rails: Fullstack framework used to build apps on front end and back end.
 
-3. Database migrations:
+3. Database migrations: process of transfering data from one or more databases to one or more chosen databases using a service.
 
-4. Params:
+4. Params: A keyword where no other additional parameters are allowed. Shorthand for method parameter.
 
-5. ERB:
+5. ERB: returns string in the same code as the input string. It is useful for generic templating situations.
